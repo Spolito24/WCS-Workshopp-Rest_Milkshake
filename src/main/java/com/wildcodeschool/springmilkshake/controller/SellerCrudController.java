047099e8720ceb3a0ea047103689e3b2a042af73
Spolito24 <baptisterenier69@gmail.com>
2023-10-13 15:12:13 +0200

    fix on the SellerCrudController: have a @Controller instead of @RestController

--- src/main/java/com/wildcodeschool/springmilkshake/controller/SellerCrudController.java
+++ src/main/java/com/wildcodeschool/springmilkshake/controller/SellerCrudController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class SellerCrudController {
 
     private final SellerRepository sellerRepository;
